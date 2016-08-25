070aa4b0f5579e816f99a61a7e941e5261a600d3
Raymund <raymundl@users.noreply.github.com>
2016-08-25 16:33:47 +0800

    compact colored output, inclusion, execution

--- bin/detector.js
+++ bin/detector.js
@@ -1,26 +1,31 @@
 #!/usr/bin/env node
-
+require('colors')
 var program = require('commander');
 var walker = require('walker');
 var eachline = require('eachline');
+var exec = require('child_process').exec;
 
 program
   .version('0.0.1')
   .option('-d, --directory [directory path]', 'root directory to search from, defaults to pwd')
   .option('-p, --pattern [text pattern]', 'text pattern to find')
-  .option('-e, --exclusion [exclusion pattern]', 'exclusive file pattern')
+  .option('-e, --exclusion [exclusion pattern]', 'exclusive directory or file pattern')
+  .option('-i, --inclusion [inclusion pattern]', 'inclusive file pattern after exclusion')
   .option('-v, --verbose', 'verbose output')
+  .option('-x, --execute [cmd]', 'execute "cmd [file name]" if text pattern matched')
   .parse(process.argv);
 
 if (!program.pattern) {
-  console.log('you must provide a text pattern');
+  console.log('\nYou must provide a text pattern. Use -h flag for help.\n');
   return;
 }
 
-
 var directory = program.directory || process.cwd();
 var pattern = new RegExp(program.pattern);
+var inclusion = program.inclusion ? new RegExp(program.inclusion) : null;
 var exclusion = program.exclusion ? new RegExp(program.exclusion) : null;
+var progress = {walker: 0, tester: 0};
+var results = {};
 
 walker(directory)
   .filterDir(function(dir, stat) {
@@ -28,11 +33,15 @@ walker(directory)
       return false;
     }
     return true;
-  }).on('file', function(file, stat) {
+  })
+  .on('file', function(file, stat) {
     if (exclusion && exclusion.test(file)) {
       return;
     }
-    testFile(file);
+    if ((inclusion && inclusion.test(file)) || (!inclusion)) {
+      progress.walker++;
+      testFile(file);
+    }
   })
   .on('error', function(er, entry, stat) {
     console.log('Got error ' + er + ' on entry ' + entry)
@@ -42,15 +51,23 @@ walker(directory)
   })
 
 function testFile(file) {
+  var result = results[file] = [];
   var lineNum = 1;
   eachline.in(file, function(line) {
     if (pattern.test(line)) {
       if (program.verbose) {
-        console.log('%s line %d: %s', file, lineNum, line.trim());
+        result.push('\n'+lineNum+': '+line.trim());
       } else {
-        console.log('%s line %d', file.replace(directory, '.'), lineNum);
+        result.push(lineNum);
       }
     }
     lineNum++;
+  }).on('finish', function() {
+    if (result.length) {
+      console.log(file.replace(directory,'.').blue, result.join(','));
+      if (program.execute) exec(program.execute+" "+file);
+    }
+    progress.tester++;
+    if (progress.walker === progress.tester) console.log('--- all files checked ---'.green);
   })
 }
